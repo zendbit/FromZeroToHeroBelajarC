55f132875389ed9d53c5258b562c08afa05e5c6f
amru <amru.rosyada@gmail.com>
2024-03-07 00:18:59 +0700

    preprocessor derivative

--- build.sh
+++ build.sh
@@ -9,28 +9,27 @@ APP_NAME=main
 ##  jika folder bin tidak ada maka buat foldernya
 if [ ! -d $BIN_DIR ]
 then
-  mkdir $BIN_DIR
+    mkdir $BIN_DIR
 fi
 
 ##  panggil compiler
 clear
 echo ""
 echo "-> Compile source..."
-gcc -I$INCLUDE_DIR $SOURCE_DIR/*.c -o $BIN_DIR/$APP_NAME
+gcc -O3 -I$INCLUDE_DIR $SOURCE_DIR/*.c -o $BIN_DIR/$APP_NAME
 
 ##  eksekusi program jika tidak ada error
 ##  dari proses sebelumnya
 if [ $? -eq 0 ]
 then
-  clear
-  echo "-> Compile Success!"
-  echo "-> Execute $BIN_DIR/$APP_NAME"
-  echo ""
+    echo "-> Compile Success!"
+    echo "-> Execute $BIN_DIR/$APP_NAME"
+    echo ""
 
-  ##  eksekusi binary filenya
-  $BIN_DIR/$APP_NAME
+    ##  eksekusi binary filenya
+    $BIN_DIR/$APP_NAME
 else
-  echo "x-> Failed!, eww"
+    echo "x-> Failed!, eww"
 fi
 
 echo ""
--- source/main.c
+++ source/main.c
@@ -3,14 +3,35 @@
  *  executable program
  * */
 
-#include <CExtern.h>
 #include <stdio.h>
+#include <CPragmaOnce.h>
+#include <CPragmaOnce.h>
+
+#define LIMIT 5
+#undef LIMIT
+
+#ifndef LIMIT
+#define LIMIT 10
+#endif
+
+#define WIN 0
+#define MACOS 1
+#define LINUX 2
+#define BSD 3
+
+#define MAX(a, b) a > b ? a : b
 
 int main()
 {
-    printf("nilai sebelum diubah: %d\n", fragment);
-    fragment = 10;
-    printf("nilai setelah diubah: %d\n", fragment);
+    printf("check max value 4, 5: %d\n", MAX(40, 5));
+
+#if defined WIN
+    printf("%s\n", "WINDOW OS");
+#endif
+    printf("Nilai LIMIT %d\n", LIMIT);
+    for(int i = 0; i < LIMIT; i++) {
+        printf("%d\n", i);
+    }
     return 0;
 }
 
